70eb3dbdb454823d2d9b422369fd2bb72ce21925
mounika1156 <katurumounika1@gmail.com>
2026-05-18 10:27:31 +0530

    first commit

---
+++ signup.js
@@ -0,0 +1,34 @@
+
+ 
+ const themeToggle = document.getElementById("themeToggle");
+ 
+ themeToggle.addEventListener("click", () => {
+ 
+ document.body.classList.toggle("light-mode");
+ 
+ const icon = themeToggle.querySelector("i");
+ 
+ if(document.body.classList.contains("light-mode")){
+ 
+ icon.classList.remove("fa-moon");
+ icon.classList.add("fa-sun");
+ 
+ }else{
+ 
+ icon.classList.remove("fa-sun");
+ icon.classList.add("fa-moon");
+ 
+ }
+ 
+ });
+ 
+
+ 
+ const rtlToggle = document.getElementById("rtlToggle");
+ 
+ rtlToggle.addEventListener("click", () => {
+ 
+ document.body.classList.toggle("rtl");
+ 
+ });
+ 
